244b4bd1f74795aa2a72c38f4bb8fb832bdfc9e7
Masaya Kemmochi <82385253+kmmch@users.noreply.github.com>
2022-01-23 13:44:23 +0900

    コンポーネント補足

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,21 @@
 // reactのコンポーネントファイルは拡張子をjsxにするよい
+
+/**
+  コンポーネントの命名規則について
+  １点補足として、コンポーネント名は必ず先頭を大文字から初めるようにする
+
+  [正しい例]
+    App
+    SomeComponent
+  
+  [エラーとなる例]
+    app
+    someComponent
+
+  [エラーにはならないが推奨されない例]
+    Some_component
+    Some_Component
+ */
 import React from "react";
 
 const App = () => {
