a4cea7e38c0f983008b4b555567f40cc962d095a
Masaya Kemmochi <82385253+kmmch@users.noreply.github.com>
2022-01-23 13:40:15 +0900

    JSX記法

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// this is comment
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  // javascriptの中にhtmlを書く　＝ JSX記法
+  // jsx記法は一つのタグで囲う必要がある。
+  // React.Fragmentを使うと一つのタグで囲う必要がなくなる
+  return (
+    <>
+      <h1>こんにちは！</h1>
+      <p>お元気ですか？</p>
+    </>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
